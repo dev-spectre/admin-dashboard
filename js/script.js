7162a273e8579c325a4e73f0f533bc5be963e577
dev-spectre <abhishekdallasalpy@gmail.com>
2023-09-01 14:20:11 +0530

    Add content for CSS and JS exercise sections

--- js/script.js
+++ js/script.js
@@ -2,10 +2,19 @@ const navOpenButton = document.querySelector(".menu");
 navOpenButton.addEventListener("click", () => {
   const nav = document.querySelector(".nav");
   nav.classList.toggle("close");
-})
+});
 
 const navCloseButton = document.querySelector(".nav__close");
 navCloseButton.addEventListener("click", () => {
   const nav = document.querySelector(".nav");
   nav.classList.add("close");
-})
+});
+
+const collapseButtons = document.querySelectorAll(".collapse");
+collapseButtons.forEach(collapseButton => {
+  collapseButton.addEventListener("click", () => {
+    collapseButton.classList.toggle("collapsed");
+    const cardSection = collapseButton.parentElement.nextElementSibling;
+    cardSection.classList.toggle("hidden");
+  });
+});
